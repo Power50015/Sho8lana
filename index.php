ac9c6f39baf8cc8f45cbf9c67cb4d1a1ff0822fd
Power Moustafa <42242552+Power50015@users.noreply.github.com>
2018-12-15 14:50:49 +0200

    ...

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
     include $tempDir . 'header.php';
 ?>
 <!-- But Yur Code Here -->
-
+    <header>
+        <div class="container">
+            <div class="d-flex align-items-center"><span>إنجز</span> مشريعك </div>
+        </div>
+    </header>
 <?php
     include $tempDir . 'footer.php';
 ?>
