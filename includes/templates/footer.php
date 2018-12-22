c75463805d20a202094bed8b9c47e30d230fd4f8
NoorEmam <45540748+NoorEmam@users.noreply.github.com>
2018-12-22 22:52:53 +0200

    footer

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,4 +1,62 @@
+     
+                <div class="container bg-secondary ">
+                <div class="row text-white ">
+                    <div class="col py-5">
+                    <spin class="f-30">شغلانه</spin><br>
+                       <div class="mt-3">
+                         <a href="#" class=" text-white">من نحن</a><br>
+                         <a href="#" class="text-white">تواصل معنا</a><br>
+                         <a href="#" class="text-white">سياسه الاستخدام</a><br>
+                         <a href="#" class="text-white">كيف تضمن حقك</a><br>
+                         <a href="#" class="text-white">الشكاوي</a>
+                        </div>
+                    </div>
+                    
+                    
+                    <div class="col py-5">
+                    <spin class="f-30">اقسام الموقع</spin><br>    
+                       <div class="mt-3"> 
+                          <a href="#" class="text-white">برمجه</a><br>
+                          <a href="#" class="text-white">تصميم</a><br>
+                          <a href="#" class="text-white">تسويق</a><br>
+                          <a href="#" class="text-white">خدمات ترجمه</a><br>
+                          <a href="#" class="text-white">خدمات اخري</a>
+                        </div>
+                    </div>
+                  
+                    
+                    <div class="col py-5 ">
+                    <spin class="f-30">وسائل الدفع</spin><br>
+                        <div class="mt-3">
+                           <a href="#" class="text-white" style="font-size:3em;"><i class="fab fa-cc-paypal"></i></a>
+                        </div>
+                    </div>
+                   
+                    
+                    <div class="col py-5">   
+                    <spin class="f-30">تابعنا</spin><br> 
+                        <div class="mt-3">
+                          <a href="#" class="text-white" style="font-size:2em;"><i class="fab fa-facebook"></i></a>
+                          <a href="#" class="text-white" style="font-size:2em;"><i class="fab fa-twitter-square"></i></a> 
+                          <a href="#" class="text-white" style="font-size:2em;"><i class="fab fa-instagram"></i></a>
+                        <div class="mt-3">
+                    </div>
+                </div>
+            </div>  
+                    
+                    
+        <div class="container bg-secondary">
+                    
+                    
+                    
+        </div>
+            
         
+
+
+
+
+
         <script src = "<?=($jsDir)?>jquery.js" ></script>
         <script src = "<?=($jsDir)?>popper.min.js" ></script>
         <script src = "<?=($jsDir)?>bootstrap.min.js" ></script>
--- index.php
+++ index.php
@@ -20,14 +20,13 @@
         <div class="over"></div>
         <div class="container position-relative">
             <div class="d-flex align-items-center font-cairo">
-            <p class="font-color-2 f-70"> <span class="font-color-1">انجز </span>مشاريعك <br> بالاعتماد على <br> توظيف مستقلين <br> محترفين </p> 
+            <p class="font-color-2 f-70"> <span class="font-color-1 f-85 ">انجز </span>مشاريعك <br> بالاعتماد على <br> توظيف مستقلين <br> محترفين </p> 
             </div>
         </div>
         <!-- section 2-->
         <div class="bg-light">
             <div class="container ">
-            
-        
+               
             <div class="row">
                 <div class="col c">
                     <h2 ><span class="badge badge-danger "> لماذا نحن </span></h2>
@@ -37,40 +36,40 @@
                 <div class="col-md-7">
                     <div class="row">
                        <div class="col "> 
-                         <div class="card form-1 " style="width: 17rem;">
+                         <div class="card form-1 " style="width: 16rem;">
                          <i class="fas fa-walking icon2 "></i>
                          <div class="card-body">
-                         <h5 class="card-title">نفذمشاريعك بسهوله </h5>
-                         <p class="card-text">اطرح مشروعك واترك مهمة تنفيذه لأفضل خبراء الوطن العرب
+                         <h5 class="card-title text-center">نفذمشاريعك بسهوله </h5>
+                         <p class="card-text text-center">اطرح مشروعك واترك مهمة تنفيذه لأفضل خبراء الوطن العرب
                          </div>
                          </div>  
                        </div>
                        <div class="col">
-                         <div class="card form-1" style="width: 17rem;">
+                         <div class="card form-1" style="width: 16rem;">
                          <i class="fas fa-handshake icon2 "></i>
                          <div class="card-body">
-                         <h5 class="card-title">وظُف أفضل المستقلين </h5>
-                         <p class="card-text">زر ملفات المستقلين ,اطلع علي أعمالهم السابقة وظف الافضل </p>
+                         <h5 class="card-title text-center ">وظُف أفضل المستقلين </h5>
+                         <p class="card-text  text-center">زر ملفات المستقلين ,اطلع علي أعمالهم السابقة وظف الافضل </p>
                          </div>
                          </div>
                        </div>    
                    </div> 
                     <div class="row c">
                        <div class="col ">
-                          <div class="card form-1" style="width: 17rem;">
+                          <div class="card form-1" style="width: 16rem;">
                           <i class="fab fa-amazon-pay icon2"></i>
                           <div class="card-body">
-                          <h5 class="card-title">ادفع بكل أريحية  </h5>
-                          <p class="card-text">  لن تدفع سوى مقابل مايتم إنجازه من وظائف </p>
+                          <h5 class="card-title text-center ">ادفع بكل أريحية  </h5>
+                          <p class="card-text  text-center">  لن تدفع سوى مقابل مايتم إنجازه من وظائف </p>
                           </div>
                           </div>
                        </div>
                        <div class="col ">
-                          <div class="card form-1" style="width: 17rem;">
+                          <div class="card form-1" style="width: 16rem;">
                           <i class="fas fa-fighter-jet icon2"></i>
                           <div class="card-body">
-                          <h5 class="card-title">انجز أعمالك بسرعه  </h5>
-                          <p class="card-text">اختر أفضل المستقلين وأرسل له الوظيفة مباشرة   </p>
+                          <h5 class="card-title text-center">انجز أعمالك بسرعه  </h5>
+                          <p class="card-text text-center">اختر أفضل المستقلين وأرسل له الوظيفة مباشرة   </p>
                           </div>
                           </div>
                        </div>  
@@ -89,8 +88,38 @@
             
         </div>
        
-            
         
+        
+        
+        
+        
+        
+         <div class="container">
+         <div class="box">
+                <p class="font-color-2 f-10 font-cairo text-center"> <span class="font-color-1 f-30">إبحث</span>عن خدمات لتنجزها  </p> 
+                    <form>
+                        
+                        <div class="row">
+                        <div class="col">
+                        <div class="form-group">
+                           <select class="form-control bg-dark font-color-2 " id="exampleFormControlSelect1">
+                           <option >القسم/option>
+                           <option>برمجة </option>
+                           <option>تسويق </option>
+                           <option>ترجمة</option>
+                           <option>تصميم</option>
+                           </select>
+                           </div>
+                        </div>
+                        <div class="col">
+                              <button type="إبحث" class="btn btn-dark font-color-2">إبحث</button>
+
+                        </div>
+                </div>  
+             </form>
+        </div>
+        
+        </div> 
         
     </header>
 <?php
