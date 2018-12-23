f8750ff7ecf20408ffc59731e0c4f1b70ba10cea
Power Moustafa <42242552+Power50015@users.noreply.github.com>
2018-12-23 11:22:51 +0200

    تصحيح اخطاء الجيت هاب

---
+++ about.php
@@ -0,0 +1,20 @@
+<?php 
+
+    session_start();
+
+    if (isset($_SESSION['Username'])) {
+        header('Location: dashboard.php'); // Redirect To Dashboard Page
+        exit();
+    }
+
+    include 'init.php';
+
+    $pageTitle = 'الرئيسيه';
+    
+    include $tempDir . 'header.php';
+
+?>
+
+<?php
+    include $tempDir . 'footer.php';
+?>
