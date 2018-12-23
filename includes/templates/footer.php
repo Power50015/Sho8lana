44e206ef520ea5d70cc434c9c15a0cc4d467eba7
NoorEmam <45540748+NoorEmam@users.noreply.github.com>
2018-12-23 15:59:06 +0200

    ...

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,50 +1,50 @@
-<footer class=" bg-secondary ">
+<footer class=" bg-color-4 ">
     <div class="container">
         <div class="row text-white ">
-                   
-                    <div class="col-md-3 py-5 ">
+                    <div class="col-lg-3 col-sm-3 col-xs-4 py-5 ">
                     <h2 class="f-30">شغلانه</h2>
-                       <div class="mt-3">
-                         <a href="#" class=" text-white">من نحن</a><br>
-                         <a href="#" class="text-white">تواصل معنا</a><br>
-                         <a href="#" class="text-white">سياسه الاستخدام</a><br>
-                         <a href="#" class="text-white">كيف تضمن حقك</a><br>
-                         <a href="#" class="text-white">الشكاوي</a>
+                       <ul class="list-unstyled">
+                           <li><a href="#" class=" text-white">من نحن</a></li>
+                           <li><a href="#" class="text-white">تواصل معنا</a></li>
+                           <li><a href="#" class="text-white">سياسه الاستخدام</a></li>
+                           <li><a href="#" class="text-white">كيف تضمن حقك</a></li>
+                           <li><a href="#" class="text-white">الشكاوي</a>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-sm-3 col-xs-4 py-5 px-1">
+                    <h2 class="f-30">اقسام الموقع</h2>   
+                       <ul class="list-unstyled"> 
+                          <li><a href="#" class="text-white">برمجه</a></li>
+                          <li><a href="#" class="text-white">تصميم</a></li>
+                          <li><a href="#" class="text-white">تسويق</a></li>
+                          <li><a href="#" class="text-white">خدمات ترجمه</a></li>
+                          <li><a href="#" class="text-white">خدمات اخري</a></li>
+                        </ul>
+                    </div>
+                   <div class="col-lg-3 col-sm-3 col-xs-4 py-5 px-1">
+                   <h2 class="f-30">وسائل الدفع</h2>
+                        <ul class="list-unstyled"> 
+                          <li><a href="#" class="text-white" style="font-size:3em;"><i class="fab fa-cc-paypal"></i></a></li> 
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-sm-3 col-xs-4 py-5 px-1">   
+                    <h2 class="f-30">تابعنا</h2>
+                        <div>
+                           <a href="#" class="text-white mr-2" style="font-size:2.5em;"><i class="fab fa-facebook"></i></a>
+                           <a href="#" class="text-white mr-2" style="font-size:2.5em;"><i class="fab fa-twitter-square"></i></a>
+                           <a href="#" class="text-white" style="font-size:2.5em;"><i class="fab fa-instagram"></i></a>
+                        
                         </div>
                     </div>
-                    
-                    
-                    <div class="col-md-3 py-5">
-                    <spin class="f-30">اقسام الموقع</spin><br>    
-                       <div class="mt-3"> 
-                          <a href="#" class="text-white">برمجه</a><br>
-                          <a href="#" class="text-white">تصميم</a><br>
-                          <a href="#" class="text-white">تسويق</a><br>
-                          <a href="#" class="text-white">خدمات ترجمه</a><br>
-                          <a href="#" class="text-white">خدمات اخري</a>
-                        </div>
-                    </div>
-                  
-                    
-                    <div class="col-md-3 py-5 ">
-                    <spin class="f-30">وسائل الدفع</spin><br>
-                        <div class="mt-3">
-                           <a href="#" class="text-white" style="font-size:3em;"><i class="fab fa-cc-paypal"></i></a>
-                        </div>
-                    </div>
-                   
-                    
-                    <div class="col-md-3 py-5">   
-                    <spin class="f-30">تابعنا</spin><br> 
-                        <div class="mt-3">
-                          <a href="#" class="text-white mr-2" style="font-size:2.5em;"><i class="fab fa-facebook"></i></a>
-                          <a href="#" class="text-white mr-2" style="font-size:2.5em;"><i class="fab fa-twitter-square"></i></a> 
-                          <a href="#" class="text-white" style="font-size:2.5em;"><i class="fab fa-instagram"></i></a>
-                        <div class="mt-3">
-                    </div>
-                        </div>
-                </div>
-    </div>
+          </div> 
+     </div>
+    
+     <copyright >
+       <div class="bg-color-5 text-white">
+          <p class="container py-3">حقوق الملكية 2019</p> 
+       </div>
+     </copyright>
+    
 </footer>  
                     
                     
