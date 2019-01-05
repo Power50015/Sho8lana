147173e37bec1e87e8db84df320f6ec9979bb7b5
Power Moustafa <42242552+Power50015@users.noreply.github.com>
2019-01-05 16:44:45 +0200

    صفحه اضافه خدمه

--- login.php
+++ login.php
@@ -67,7 +67,7 @@
                     <div class="form-group pt-4 pb-5">
                         <a href="#" class="font-cairo"> هل نسيت كلمة السر؟</a>
                     </div>
-                    <button type="تسجيل الدخول" class="btn btn-danger d-block w-100 mb-3 py-2 font-cairo f-18">تسجيل الدخول</button>
+                    <input type="submit" value="تسجيل الدخول" name="login" class="btn btn-danger d-block w-100 mb-3 py-2 font-cairo f-18" >
                 </form>
             </div>
     </div>  
