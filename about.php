d6665d2bcab00e989ee72c0cd9c3543c61a394e9
NoorEmam <45540748+NoorEmam@users.noreply.github.com>
2019-03-01 17:56:42 +0200

    تعديل
    .......

--- about.php
+++ about.php
@@ -37,18 +37,13 @@
 </section>
 <section class="bg-color-12">
 <div class="container">
-    
-    <!--start row1-->
-    <div class="row">
      <h2 class="bg-color-3 d-inline-block mt-5 px-3 py-1 f-30 font-cairo text-light"> الفريق </h2>
-    </div>
-    <!--end row1-->
     
-    <!-- start row2-->
     <div class="row justify-content-around mt-5 ">   
-    <div class="bg-color-7  col-lg-5 col-sm-12 ">
+    <div class=" col-lg-5 col-sm-12 ">
         
-      <div class="row  px-2 py-2"> 
+    <!--start 1-->
+    <div class="bg-color-7 row  px-2 py-2"> 
       <div class="col-8">
         
          <h5 class=" bg-color-1 px-3 py-1 mt-2 d-inline-block font-cairo ">
@@ -57,81 +52,22 @@
           <p class="bg-color-1 d-inline-block px-3 py-1 mt-2 font-cairo">  هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث ي </p> 
 
       </div>
-      <div class="col-3">
+      <div class="col-3 mr-2">
           <img src="upload\3.jpg" class="rounded-circle w-100 h-100 position-relative" alt="..."> 
       </div>
-          
-    </div>
-     <div class="row justify-content-md-center">
         <ul>
              <li class=" d-inline"><a href="https://fb.com" class="text-muted mr-2 f-2" ><i class="fab fa-facebook"></i></a></li>
              <li class="d-inline"><a href="https://twitter.com" class="text-muted mr-2 f-2 " ><i class="fab fa-twitter-square"></i></a></li>
              <li class="d-inline" ><a href="https://instagram.com" class="text-muted f-2" ><i class=" fab fa-instagram"></i></a></li>           
          </ul> 
         
-        </div>   
-    
-        </div>   
-    <div class="bg-color-7 col-lg-5 col-sm-12">
-        
-      <div class="row  px-2 py-2"> 
-      <div class="col-8">
-        
-         <h5 class=" bg-color-1 px-3 py-1 mt-2 d-inline-block font-cairo ">
-          محمد مصطفي  
-          </h5>  
-          <p class="bg-color-1 d-inline-block px-3 py-1 mt-2 font-cairo">  هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث ي </p> 
-
-      </div>
-      <div class="col-3">
-          <img src="upload\1.jpg" class="rounded-circle w-100 h-100 position-relative" alt="..."> 
-      </div>
-          
+             
     </div>
-     <div class="row justify-content-md-center">
-        <ul>
-             <li class=" d-inline"><a href="https://fb.com" class="text-muted mr-2 f-2" ><i class="fab fa-facebook"></i></a></li>
-             <li class="d-inline"><a href="https://twitter.com" class="text-muted mr-2 f-2 " ><i class="fab fa-twitter-square"></i></a></li>
-             <li class="d-inline" ><a href="https://instagram.com" class="text-muted f-2" ><i class=" fab fa-instagram"></i></a></li>           
-         </ul> 
-        
-        </div>   
-        </div>
-    </div>
-
-    <!--end row2-->
     
-    <!-- start row3-->
-    <div class="row justify-content-around mt-5 ">   
-    <div class="bg-color-7  col-lg-5 col-sm-12">
+    <!-- end 1-->
         
-      <div class="row  px-2 py-2"> 
-      <div class="col-8">
-        
-         <h5 class=" bg-color-1 px-3 py-1 mt-2 d-inline-block font-cairo ">
-            مصطفي محمود 
-          </h5>  
-          <p class="bg-color-1 d-inline-block px-3 py-1 mt-2 font-cairo">  هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث ي </p> 
-
-      </div>
-      <div class="col-3">
-          <img src="upload\4.jpg" class="rounded-circle w-100 h-100 position-relative" alt="..."> 
-      </div>
-          
-    </div>
-     <div class="row justify-content-md-center">
-        <ul>
-             <li class=" d-inline"><a href="https://fb.com" class="text-muted mr-2 f-2" ><i class="fab fa-facebook"></i></a></li>
-             <li class="d-inline"><a href="https://twitter.com" class="text-muted mr-2 f-2 " ><i class="fab fa-twitter-square"></i></a></li>
-             <li class="d-inline" ><a href="https://instagram.com" class="text-muted f-2" ><i class=" fab fa-instagram"></i></a></li>           
-         </ul> 
-        
-        </div>   
-    
-        </div>   
-    <div class="bg-color-7 col-lg-5 col-sm-12">
-        
-      <div class="row  px-2 py-2"> 
+    <!-- star 2-->
+    <div class="bg-color-7 row  px-2 py-2 mt-5"> 
       <div class="col-8">
         
          <h5 class=" bg-color-1 px-3 py-1 mt-2 d-inline-block font-cairo ">
@@ -143,26 +79,16 @@
       <div class="col-3">
           <img src="upload\2.jpg" class="rounded-circle w-100 h-100 position-relative" alt="..."> 
       </div>
-          
-    </div>
-     <div class="row justify-content-md-center">
         <ul>
              <li class=" d-inline"><a href="https://fb.com" class="text-muted mr-2 f-2" ><i class="fab fa-facebook"></i></a></li>
              <li class="d-inline"><a href="https://twitter.com" class="text-muted mr-2 f-2 " ><i class="fab fa-twitter-square"></i></a></li>
              <li class="d-inline" ><a href="https://instagram.com" class="text-muted f-2" ><i class=" fab fa-instagram"></i></a></li>           
          </ul> 
-        
         </div>   
-        </div>
-    </div>
-
-    <!--end row3-->
-    
-    <!-- start row4-->
-    <div class="row justify-content-around mt-5 ">   
-    <div class="bg-color-7  col-lg-5 col-sm-12">
-        
-      <div class="row  px-2 py-2"> 
+   <!-- end 2-->
+   
+   <!-- start 3-->
+    <div class="bg-color-7 row  px-2 py-2 mt-5"> 
       <div class="col-8">
         
          <h5 class=" bg-color-1 px-3 py-1 mt-2 d-inline-block font-cairo ">
@@ -174,9 +100,6 @@
       <div class="col-3">
           <img src="upload\5.jpg" class="rounded-circle w-100 h-100 position-relative" alt="..."> 
       </div>
-          
-    </div>
-     <div class="row justify-content-md-center">
         <ul>
              <li class=" d-inline"><a href="https://fb.com" class="text-muted mr-2 f-2" ><i class="fab fa-facebook"></i></a></li>
              <li class="d-inline"><a href="https://twitter.com" class="text-muted mr-2 f-2 " ><i class="fab fa-twitter-square"></i></a></li>
@@ -184,15 +107,62 @@
          </ul> 
         
         </div>   
-    
-        </div>   
-    <div class="bg-color-7 col-lg-5 col-sm-12">
+    <!-- end 3-->
+        </div>      
+      
+    <div class="col-lg-5 col-sm-12 ">
         
-      <div class="row  px-2 py-2"> 
+    <!--start 1-->
+    <div class="bg-color-7 row  px-2 py-2"> 
       <div class="col-8">
         
          <h5 class=" bg-color-1 px-3 py-1 mt-2 d-inline-block font-cairo ">
-          أسراء محمود  
+         محمد مصطفي   
+ 
+          </h5>  
+          <p class="bg-color-1 d-inline-block px-3 py-1 mt-2 font-cairo">  هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث ي </p> 
+
+      </div>
+      <div class="col-3 mr-2">
+          <img src="upload\1.jpg" class="rounded-circle w-100 h-100 position-relative" alt="..."> 
+      </div>
+        <ul>
+             <li class=" d-inline"><a href="https://fb.com" class="text-muted mr-2 f-2" ><i class="fab fa-facebook"></i></a></li>
+             <li class="d-inline"><a href="https://twitter.com" class="text-muted mr-2 f-2 " ><i class="fab fa-twitter-square"></i></a></li>
+             <li class="d-inline" ><a href="https://instagram.com" class="text-muted f-2" ><i class=" fab fa-instagram"></i></a></li>           
+         </ul> 
+        
+        </div>   
+    <!-- end 1-->
+        
+    <!-- star 2-->
+    <div class="bg-color-7 row  px-2 py-2 mt-5"> 
+      <div class="col-8">
+        
+         <h5 class=" bg-color-1 px-3 py-1 mt-2 d-inline-block font-cairo ">
+         مصطفي محمود 
+          </h5>  
+          <p class="bg-color-1 d-inline-block px-3 py-1 mt-2 font-cairo">  هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث ي </p> 
+
+      </div>
+      <div class="col-3">
+          <img src="upload\4.jpg" class="rounded-circle w-100 h-100 position-relative" alt="..."> 
+      </div>
+        <ul>
+             <li class=" d-inline"><a href="https://fb.com" class="text-muted mr-2 f-2" ><i class="fab fa-facebook"></i></a></li>
+             <li class="d-inline"><a href="https://twitter.com" class="text-muted mr-2 f-2 " ><i class="fab fa-twitter-square"></i></a></li>
+             <li class="d-inline" ><a href="https://instagram.com" class="text-muted f-2" ><i class=" fab fa-instagram"></i></a></li>           
+         </ul> 
+        
+        </div>   
+   <!-- end 2-->
+   
+   <!-- start 3-->
+    <div class="bg-color-7 row  px-2 py-2 mt-5"> 
+      <div class="col-8">
+        
+         <h5 class=" bg-color-1 px-3 py-1 mt-2 d-inline-block font-cairo ">
+             اسراء محمود  
           </h5>  
           <p class="bg-color-1 d-inline-block px-3 py-1 mt-2 font-cairo">  هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث ي </p> 
 
@@ -200,9 +170,6 @@
       <div class="col-3">
           <img src="upload\6.jpg" class="rounded-circle w-100 h-100 position-relative" alt="..."> 
       </div>
-          
-    </div>
-     <div class="row justify-content-md-center">
         <ul>
              <li class=" d-inline"><a href="https://fb.com" class="text-muted mr-2 f-2" ><i class="fab fa-facebook"></i></a></li>
              <li class="d-inline"><a href="https://twitter.com" class="text-muted mr-2 f-2 " ><i class="fab fa-twitter-square"></i></a></li>
@@ -210,12 +177,9 @@
          </ul> 
         
         </div>   
-        </div>
+    <!-- end 3-->
+        </div>    
     </div>
-
-    <!--end row4-->
-    
-    <!-- start row4-->
     <div class="row justify-content-around mt-5  ">   
     <div class="bg-color-7  col-lg-5 col-sm-12 mb-5">
         
@@ -232,8 +196,7 @@
           <img src="upload\3c1f3f3aead0ac7e009ee9952e131647.png" class="rounded-circle w-100 h-100 position-relative" alt="..."> 
       </div>
           
-    </div>
-     <div class="row justify-content-md-center">
+    
         <ul>
              <li class=" d-inline"><a href="https://fb.com" class="text-muted mr-2 f-2" ><i class="fab fa-facebook"></i></a></li>
              <li class="d-inline"><a href="https://twitter.com" class="text-muted mr-2 f-2 " ><i class="fab fa-twitter-square"></i></a></li>
@@ -245,12 +208,12 @@
         </div>   
     </div>
 
-    <!--end row4-->
-    
-    
     </div>
-
 </section>
+
+    
+
+
 <?php
     include $tempDir . 'footer.php';
 ?>
